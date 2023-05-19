7ce8e542a1a545a5fdb6d3067d68c00e6ce438a3
Elvis Njoroge Gitau <elvis.gitau@student.moringaschool.com>
2023-05-19 19:21:50 +0300

    first commit

---
+++ src/components/Skills.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+
+import HTML from '../assets/html.png';
+import CSS from '../assets/css.png';
+import JavaScript from '../assets/javascript.png';
+import ReactImg from '../assets/react.png';
+import postgresql from '../assets/postgresql.png';
+import Ruby from '../assets/Ruby.png';
+import GitHub from '../assets/github.png';
+import Tailwind from '../assets/tailwind.png';
+import Ruby_on_Rails from '../assets/Ruby_On_Rails.png';
+
+const Skills = () => {
+  return (
+    <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+      {/* Container */}
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+          <div>
+              <p className='text-4xl font-bold inline border-b-4 border-pink-600 '>Skills</p>
+              <p className='py-4'> These are the technologies I have worked with</p>
+          </div>
+
+          <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={HTML} alt="HTML icon" />
+                  <p className='my-4'>HTML</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={CSS} alt="HTML icon" />
+                  <p className='my-4'>CSS</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={JavaScript} alt="HTML icon" />
+                  <p className='my-4'>JAVASCRIPT</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={ReactImg} alt="HTML icon" />
+                  <p className='my-4'>REACT</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={GitHub} alt="HTML icon" />
+                  <p className='my-4'>GITHUB</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={postgresql} alt="HTML icon" />
+                  <p className='my-4'>POSTGRESQL</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={Ruby_on_Rails} alt="HTML icon" />
+                  <p className='my-4'>RUBY ON RAILS</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={Tailwind} alt="HTML icon" />
+                  <p className='my-4'>TAILWIND</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={Ruby} alt="HTML icon" />
+                  <p className='my-4'>RUBY</p>
+              </div>
+          </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
---
+++ src/data/data.js
@@ -0,0 +1,39 @@
+import WorkIm from '../assets/projects/workImg.jpeg'
+import RealEst from '../assets/projects/realestate.jpg'
+
+export const data=[
+    {
+        id:1,
+        name:"React JS Application",
+        image:WorkIm,
+        github:"",
+        description:"",
+        live:"",
+    },
+    {
+        id:2,
+        name:"React JS Application",
+        image:RealEst,
+        github:"",
+        description:"",
+        live:"",
+    },
+    {
+        id:3,
+        name:"React JS Application",
+        image:WorkIm,
+        github:"",
+        description:"",
+        live:"",
+    },
+    {
+        id:4,
+        name:"React JS Application",
+        image:RealEst,
+        github:"",
+        description:"",
+        live:"",
+    },
+
+
+]
