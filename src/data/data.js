bf17987f917016eb54e0eedeb28c543166e4fae6
Elvis Njoroge Gitau <elvis.gitau@student.moringaschool.com>
2023-06-05 21:32:02 +0300

    add typefast game live link

--- src/data/data.js
+++ src/data/data.js
@@ -1,12 +1,14 @@
 import WorkIm from '../assets/projects/tulewapi.jpg'
 import RealEst from '../assets/projects/realestate.jpg'
+import typefast from '../assets/projects/typefast.png'
 
 export const data=[
     {
         id:1,
-        name:"Tulewapi Restaurant Advisor",
-        image:WorkIm,
-        github:"https://github.com/Elvis-Njoroge/tulewapi",
+        name:"TypeFast Typing Game",
+        image:typefast,
+        github:"https://github.com/Elvis-Njoroge/typefast",
+        live:"https://elvis-njoroge.github.io/typefast/",
         description:"Live link coming soon"
     }
 
