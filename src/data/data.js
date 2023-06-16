0f117614d00642d15ac005322542ba7c2a53fd53
Elvis Njoroge Gitau <elvis.gitau@student.moringaschool.com>
2023-06-16 12:04:21 +0300

    add jipange frontend link

--- src/data/data.js
+++ src/data/data.js
@@ -15,7 +15,7 @@ export const data=[
         name:"Jipange Personal Finanace",
         image:jipange,
         github:"https://github.com/Elvis-Njoroge/jipange",
-        live:"/",
+        live:"https://jipange.vercel.app/",
         description:"A personal finance app where users can manage their assets, financial goals, expenses and budgets. Buiilt on a React App frontend and a Ruby on Rails backend, using a postrgreql database. Live link coming soon. You can have a sneak peak by downloading the zipfile on the github repo and following the instructions on the readme file"
     }
 ]
